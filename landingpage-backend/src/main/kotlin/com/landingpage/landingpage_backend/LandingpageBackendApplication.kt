f08f22c035999509230ca860d09ce7b8c6d7dac0
BlackNazza <Ganidopvzgw@gmail.com>
2025-06-25 02:59:50 +0200

    init

---
+++ landingpage-backend/src/main/kotlin/com/landingpage/landingpage_backend/LandingpageBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.landingpage.landingpage_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LandingpageBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LandingpageBackendApplication>(*args)
+}
